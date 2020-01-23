6d2ff7be31a821bb8a3265f9a406857ce514dbd1
nayeemahmed24 <42116405+nayeemahmed24@users.noreply.github.com>
2020-01-23 14:31:32 +0600

    Added Subtraction that implemented subtraction

---
+++ DTD.Calculator.Core/Operations/Subtraction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTD.Calculator.Core.Interfaces;
+
+namespace DTD.Calculator.Core.Operations
+{
+    internal class Subtraction<T> : IOperation<T>
+    {
+        public T Operate(T left, T right)
+        {
+            return (dynamic) left + (dynamic) right;
+        }
+    }
+}
