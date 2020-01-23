484c57e1ce014730ae8f5c864fc1e5f46f2f1c1c
Tanimul Haque Khan <tanimul.haque@bs-23.net>
2020-01-23 16:13:59 +0600

    Added Factory Class

---
+++ DTD.Calculator.Core/Factory/OperationFactory.cs
@@ -0,0 +1,33 @@
+﻿using DTD.Calculator.Core.Operations;
+
+namespace DTD.Calculator.Core.Factory
+{
+    public class OperationFactory<T>
+    {
+        public enum Operators
+        {
+            Plus,Minus,Multiplication,Division
+        }
+
+        public virtual IOperation<T> GetOperation(Operators _operator)
+        {
+            switch (_operator)
+            {
+                case Operators.Plus:
+                    return new Addition<T>();
+                case Operators.Minus:
+                    return new Subtraction<T>();
+                case Operators.Multiplication:
+                    return new Multiplication<T>();
+                case Operators.Division:
+                    return new Division<T>();
+                default:
+                    return null;
+            }
+        }
+
+
+    }
+
+ 
+}
